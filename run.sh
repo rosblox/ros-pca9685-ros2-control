9f49daa02aab365d865b9907ab519aa2d1c56f85
Max Polzin <polzin.max@gmail.com>
2023-06-10 22:47:21 +0200

    Modify run script.

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-docker run -it --net=host --device /dev/i2c-1:/dev/i2c-1 --volume $(pwd)/pca9685_ros2_control:/colcon_ws/src/pca9685_ros2_control ghcr.io/rosblox/ros-pca9685-ros2-control:humble
+#!/bin/bash
+
+docker run -it --net=host --device=/dev/i2c-1:/dev/i2c-1 --volume $(pwd)/pca9685_ros2_control:/colcon_ws/src/pca9685_ros2_control ghcr.io/rosblox/ros-pca9685-ros2-control:humble
