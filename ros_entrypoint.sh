20cb94dd0a176e56f404c2953ca36100050cbb54
Max Polzin <polzin.max@gmail.com>
2023-08-08 13:33:31 +0200

    Create group if it doesnt exist.

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} --uid ${GID:=1000} ros
 
-groupadd -g 998 i2c
+getent group i2c || groupadd -g 998 i2c
 usermod -aG i2c ros
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
