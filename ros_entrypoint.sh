9c4ffd0b33754bfb6a486c1bbbf31b18e7da6159
Max Polzin <polzin.max@gmail.com>
2025-05-07 19:05:16 +0200

    Remove dupli

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} --uid ${GID:=1000} ros
+id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} ros
 
 getent group i2c || groupadd -g 998 i2c
 usermod -aG i2c ros
